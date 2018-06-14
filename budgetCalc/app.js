4f51743e8ae4c7dee16050eabdbbc19b35b704ed
Msarbel <35122452+msarbel@users.noreply.github.com>
2018-06-13 20:54:55 -0400

    Updates
    Updated the UI to allow for DOM updating of a new budget in both income and expense when a user enters values and submits. Also added the ability for the init function to update the UI to 0 when a new page is opened or refreshed (current) will look for localStorage to save data of budget for month

--- budgetCalc/app.js
+++ budgetCalc/app.js
@@ -117,7 +117,11 @@ var UIController = (function() {
       inputValue: '.add__value',
       inputBtn: '.add__btn',
       incomeContainer: '.income__list',
-      expensesContainer: '.expenses__list'
+      expensesContainer: '.expenses__list',
+      budgetLabel: '.budget__value',
+      incomeLabel: '.budget__income--value',
+      expensesLabel: '.budget__expenses--value',
+      percentageLabel: '.budget__expenses--percentage'
   };
 
   return {
@@ -171,6 +175,20 @@ var UIController = (function() {
 
     },
 
+    displayBudget: function(obj) {
+
+        document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+        document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+        document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+        if(obj.percentage > 0) {
+          document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + "%";
+        } else {
+          document.querySelector(DOMstrings.percentageLabel).textContent = '----';
+        }
+
+    },
+
 
     getDOMstrings: function() {
       return DOMstrings;
@@ -208,7 +226,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         let budget = budgetCtrl.getBudget();
 
         // display the budget on UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
 
    };
 
@@ -239,6 +257,12 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
         console.log('app has started');
+        UICtrl.displayBudget(
+            {budget: 0,
+            totalInc: 0,
+            totalExp: 0,
+            percentage: -1
+          });
         setupEventListeners();
     }
   };
